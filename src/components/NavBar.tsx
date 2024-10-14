c116c21b0bb7999417024b09e34fe536415cbdf4
owenygy <owenyi227@gmail.com>
2024-10-15 10:59:59 +1100

    Building game cards

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,12 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
     <div>
-      <HStack justifyContent={"space-between"} p={4} padding={"0 20px"}>
-        <Image src={logo} boxSize={"80px"} />
+      <HStack justifyContent={"space-between"} p={4} padding={"0 10px"}>
+        <Image src={logo} boxSize={"60px"} />
         <ColorModeSwitch />
       </HStack>
     </div>
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    params: {
-        base_url: "https://api.rawg.io/api",
-        api_key: "bfc24e80217242f7baa31884f63d3925",
-    },
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "bfc24e80217242f7baa31884f63d3925",
+  },
 });
